9febe5ca1d5b750649913a1ead20a8487616ba24
Debopriyo Basu <debopriyobasu@gmail.com>
2024-05-01 09:26:54 +0530

    created home page layout and styles

---
+++ property-pulse/app/layout.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "@/assets/styles/globals.css";
+
+export const metadata = {
+  title: "PropertyPulse | Find the perfect rental",
+  description: "Find your dream rental property",
+};
+const MainLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div>{children}</div>
+      </body>
+    </html>
+  );
+};
+
+export default MainLayout;
