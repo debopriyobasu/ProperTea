5606dc4cab4caaa8401ece6af6f6fb484b46a9e5
Debopriyo Basu <debopriyobasu@gmail.com>
2024-08-06 11:05:47 +0530

    feat: cloudinary image integration

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -2,6 +2,7 @@ import connectDB from "@/config/database";
 import Property from "@/models/Property";
 import PropertyDetails from "@/components/PropertyDetails";
 import PropertyHeaderImage from "@/components/PropertyHeaderImage";
+import PropertyImages from "@/components/PropertyImages";
 import Link from "next/link";
 import { FaArrowLeft } from "react-icons/fa";
 const PropertyPage = async ({ params }) => {
@@ -20,13 +21,14 @@ const PropertyPage = async ({ params }) => {
           </Link>
         </div>
       </section>
-      <section class="bg-blue-50">
-        <div class="container m-auto py-10 px-6">
-          <div class="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
+      <section className="bg-blue-50">
+        <div className="container m-auto py-10 px-6">
+          <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
             <PropertyDetails property={property} />
           </div>
         </div>
       </section>
+      <PropertyImages images={property.images} />
     </>
   );
 };
