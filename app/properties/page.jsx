716b73e54f1e451cd1068a692ca1f65a6d5c02fd
Debopriyo Basu <debopriyobasu@gmail.com>
2024-08-14 08:44:12 +0530

    feat: added pagination and photoswipe lightbox

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,10 +1,14 @@
 import PropertyCard from "@/components/PropertyCard";
 import connectDB from "@/config/database";
 import Property from "@/models/Property";
+import Pagination from "@/components/Pagination";
 
-const PropertiesPage = async () => {
+const PropertiesPage = async ({ searchParams: { page = 1, pageSize = 9 } }) => {
   await connectDB();
-  const properties = await Property.find({}).lean();
+  const skip = (page - 1) * pageSize;
+  const total = await Property.countDocuments({});
+  const showPagination = total > pageSize;
+  const properties = await Property.find({}).skip(skip).limit(pageSize);
   return (
     <section className="px-4 py-6">
       <div className="container-xl lg:container m-auto px-4 py-6">
@@ -18,6 +22,13 @@ const PropertiesPage = async () => {
           </div>
         )}
       </div>
+      {showPagination && (
+        <Pagination
+          page={parseInt(page)}
+          pageSize={parseInt(pageSize)}
+          totalItems={total}
+        />
+      )}
     </section>
   );
 };
